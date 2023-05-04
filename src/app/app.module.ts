5e7eeaedb4d38761669482cd88ccd726ceaa8769
nicolas <nicolas.fontaine@toulouse.miage.fr>
2023-05-04 11:40:04 +0200

    page d'accueil v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,12 @@ import { EvenementComponent } from './evenement/evenement.component';
 import { PersonneComponent } from './personne/personne.component';
 import {FormsModule} from "@angular/forms";
 import {Router, RouterModule, Routes} from "@angular/router";
-import { PageAccueilComponent } from './page-accueil/page-accueil.component';
+//import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 const appRoutes:Routes = [
   // 1 route par module afin de charger les pages
   {path : 'lister-evt', component: ListerEventComponent },
-  {path : 'evenement', component: EvenementComponent },
-  {path : 'page-accueil', component: PageAccueilComponent}
+  {path : 'evenement', component: EvenementComponent }
+  //{path : 'page-accueil', component: PageAccueilComponent}
 ];
 @NgModule({
   declarations: [
@@ -22,8 +22,8 @@ const appRoutes:Routes = [
     ListerEventComponent,
     InscriptionComponent,
     EvenementComponent,
-    PersonneComponent,
-    PageAccueilComponent
+    PersonneComponent
+    //PageAccueilComponent
   ],
   imports: [
     BrowserModule, NgbModule, RouterModule.forRoot(appRoutes), FormsModule
