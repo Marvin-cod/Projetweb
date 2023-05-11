d759786588c10a8f8190569b3b5342a1023f075a
Marvinwgnr <marvin.wagner@toulouse.miage.fr>
2023-05-11 09:46:31 +0200

    Ajout des personnes

--- src/app/api-evenements.service.ts
+++ src/app/api-evenements.service.ts
@@ -2,31 +2,38 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Evenement} from "./Evenement";
 import {Observable} from "rxjs";
+import {Personne} from "./Personne";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiEvenementsService {
-  private url = 'http://localhost:3000/api/evenements';
+  private urlevent = 'http://localhost:3000/api/evenements';
+  private urlpersonne = 'http://localhost:3000/api/personnes';
   constructor(private httpClient: HttpClient) { }
 
   public recupererliste() : Observable<Evenement[]>{  // coté client les erreurs
-    return this.httpClient.get<Evenement[]>(this.url);
+    return this.httpClient.get<Evenement[]>(this.urlevent);
   }
   public getEvenement(id : number): Observable<Evenement>{
-    return this.httpClient.get<Evenement>(this.url + '/'+ id);
+    return this.httpClient.get<Evenement>(this.urlevent + '/'+ id);
   }
   public supprimerEvenement(id : number){
-    this.httpClient.delete<Evenement>(this.url +'/'+id).subscribe(
+    this.httpClient.delete<Evenement>(this.urlevent +'/'+id).subscribe(
       (reponse : Evenement)=> {console.log(reponse);},
       (error)=>{console.log("erreur lors de l'ajout")}
     );
   }
   public ajouterEvenement(event: Evenement){
-    this.httpClient.post<Evenement>(this.url, event).subscribe(
+    this.httpClient.post<Evenement>(this.urlevent, event).subscribe(
       (reponse : Evenement)=> {console.log(reponse);},
       (error)=>{console.log("erreur lors de l'ajout")}
     )
 
+  }
+  public ajouterPersonne(personne : Personne){
+    this.httpClient.post<Personne>(this.urlpersonne, personne).subscribe(
+      (reponse : Personne)=> {console.log(reponse);},
+      (error)=>{console.log("erreur lors de l'ajout")})
   }
 }
--- src/app/inscription-event/inscription-event.component.ts
+++ src/app/inscription-event/inscription-event.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ApiEvenementsService} from "../api-evenements.service";
+import {HttpClient} from "@angular/common/http";
+import {Personne} from "../Personne";
+import {Evenement} from "../Evenement";
 
 @Component({
   selector: 'app-inscription-event',
   templateUrl: './inscription-event.component.html',
   styleUrls: ['./inscription-event.component.css']
 })
-export class InscriptionEventComponent {
-
+export class InscriptionEventComponent implements OnInit{
+  // @ts-ignore
+  personnes : Personne;
+  constructor(private apiEvenementService : ApiEvenementsService,
+              private httpClient: HttpClient) {
+  }
+  ngOnInit() {
+    this.personnes = new Personne();
+  }
+  ajouterPersonne(){
+    this.apiEvenementService.ajouterPersonne(this.personnes);
+  }
 }
