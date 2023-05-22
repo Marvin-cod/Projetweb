4033820d158e456c79817767a1e06d0cce05635e
Marvinwgnr <marvin.wagner@toulouse.miage.fr>
2023-05-22 15:28:00 +0200

    version

--- src/app/ajouter-event/ajouter-event.component.ts
+++ src/app/ajouter-event/ajouter-event.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ApiEvenementsService} from "../api-evenements.service";
 import {HttpClient} from "@angular/common/http";
 import {Evenement} from "../Evenement";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-ajouter-event',
@@ -12,9 +13,14 @@ export class AjouterEventComponent implements OnInit {
 
   // @ts-ignore
   evenement: Evenement;
+  // @ts-ignore
+  msg : string;
+  // @ts-ignore
+  msgG : String;
 
   constructor(private apiEvenementService: ApiEvenementsService,
-              private httpClient: HttpClient) {
+              private httpClient: HttpClient,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -22,9 +28,27 @@ export class AjouterEventComponent implements OnInit {
   }
 
   ajouter() {
+
     let dateTemp = this.evenement.date_cloture.split("-"); // chaîne de date au format yyyy-mm-dd, sépare la chaîne en parties (année, mois, jour)
     this.evenement.date_cloture = dateTemp.reverse().join("/"); // inverse l'ordre des parties et les joint avec des "/"
     // format dd/mm/yyyy
-    this.apiEvenementService.ajouterEvenement(this.evenement)
+    if (this.evenement.nom_event == undefined || this.evenement.lieu_event == undefined){
+      this.msgG = "Vous n'avez pas rempli tous les champs obligatoires";
+    }
+    else if (this.verifDate(this.evenement.date_cloture, this.evenement.date_ouverture)){
+      this.router.navigate(['lister-evt']);
+      this.apiEvenementService.ajouterEvenement(this.evenement)
+    }
+    else{
+      this.msg="Mauvaise date";
+    }
+  }
+  public verifDate(dateCloture : string, dateOuverture : string){
+    let dateCtemp :Date = new Date (dateCloture);
+    let dateOtemp : Date = new Date(dateOuverture);
+    if (dateOtemp > dateCtemp){
+      return false;
+    }
+    return true;
   }
 }
