e64a577ad748e55071a457e0aaa5b7ffc271810e
Marvinwgnr <marvin.wagner@toulouse.miage.fr>
2023-05-22 16:37:35 +0200

    version

--- src/app/ajouter-event/ajouter-event.component.ts
+++ src/app/ajouter-event/ajouter-event.component.ts
@@ -16,7 +16,9 @@ export class AjouterEventComponent implements OnInit {
   // @ts-ignore
   msg : string;
   // @ts-ignore
-  msgG : String;
+  msgA : string;
+  // @ts-ignore
+  msgN : string;
 
   constructor(private apiEvenementService: ApiEvenementsService,
               private httpClient: HttpClient,
@@ -32,19 +34,25 @@ export class AjouterEventComponent implements OnInit {
     let dateTemp = this.evenement.date_cloture.split("-"); // chaîne de date au format yyyy-mm-dd, sépare la chaîne en parties (année, mois, jour)
     this.evenement.date_cloture = dateTemp.reverse().join("/"); // inverse l'ordre des parties et les joint avec des "/"
     // format dd/mm/yyyy
-    if (this.evenement.nom_event == undefined || this.evenement.lieu_event == undefined){
-      this.msgG = "Vous n'avez pas rempli tous les champs obligatoires";
-    }
-    else if (this.verifDate(this.evenement.date_cloture, this.evenement.date_ouverture)){
+    if (this.verifDate(this.evenement.date_cloture, this.evenement.date_ouverture) && this.evenement.acronyme!=undefined && this.evenement.nombre_max>0){
       this.router.navigate(['lister-evt']);
-      this.apiEvenementService.ajouterEvenement(this.evenement)
+      this.apiEvenementService.ajouterEvenement(this.evenement);
+
     }
     else{
-      this.msg="Mauvaise date";
+      if (this.evenement.acronyme==undefined){
+        this.msgA="Veuillez ajouter l'acronyme";
+      }
+      if (!this.verifDate(this.evenement.date_cloture, this.evenement.date_ouverture)) {
+        this.msg = "Mauvaise date";
+      }
+      if(this.evenement.nombre_max>0 || this.evenement.nombre_max==undefined){
+        this.msgN="Veuillez saisir un nombre positif de participants max";
+      }
     }
   }
   public verifDate(dateCloture : string, dateOuverture : string){
-    let dateCtemp :Date = new Date (dateCloture);
+    let dateCtemp : Date = new Date (dateCloture);
     let dateOtemp : Date = new Date(dateOuverture);
     if (dateOtemp > dateCtemp){
       return false;
